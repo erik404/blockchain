be8f52793fdc619191084d42c748e1ccb0622ac2
Erik-Jan van de Wal <erikfourofour@proton.me>
2025-01-22 02:34:14 -0600

    rename error message

--- src/errors/transaction_errors.rs
+++ src/errors/transaction_errors.rs
@@ -1,6 +1,6 @@
 #[derive(Debug)]
 pub enum TransactionError {
-    AddressesCannotBeEmpty,
+    AddressCannotBeEmpty,
     SenderAndReceiverCannotBeTheSame,
     AmountMustBeGreaterThanZero,
     InsufficientBalance {
@@ -16,11 +16,11 @@ pub enum TransactionError {
 impl std::fmt::Display for TransactionError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            TransactionError::AddressesCannotBeEmpty => {
-                write!(f, "Transaction addresses cannot be empty.")
+            TransactionError::AddressCannotBeEmpty => {
+                write!(f, "Transaction address cannot be empty.")
             }
             TransactionError::SenderAndReceiverCannotBeTheSame => {
-                write!(f, "Sender and Receiver cannot be the same.")
+                write!(f, "Sender and receiver cannot be the same.")
             }
             TransactionError::AmountMustBeGreaterThanZero => {
                 write!(f, "Transaction must be greater than zero.")
