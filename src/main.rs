f61f4b7fe5375f4622d953b9b6a48ee680b11519
Erik-Jan van de Wal <erikfourofour@proton.me>
2025-01-28 05:42:23 -0600

    test file

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,18 @@ use crate::config::Config;
 use crate::core::blockchain::Blockchain;
 use crate::structs::transaction::Transaction;
 
-pub mod tests {
-    pub mod helpers;
-}
+#[cfg(test)]
+mod test_utils;
 
 pub mod errors {
     pub mod transaction_errors;
 }
 
 pub mod config;
+
+mod common {
+    pub mod calculate_hash;
+}
 
 pub mod core {
     pub mod block;
@@ -22,7 +25,7 @@ pub mod structs {
     pub mod transaction;
 }
 fn main() {
-    let config: Config = Config::load();
+    let config: Config = Config::load().unwrap();
     let mut blockchain: Blockchain = Blockchain::new(config).unwrap();
 
     blockchain
@@ -30,28 +33,29 @@ fn main() {
         .insert("Alice".to_string(), 1_000_000_000); // 10 LTK
     blockchain.accounts.insert("Bob".to_string(), 2_500_000_000); // 25 LTK
 
-
     // Initialize balances
     blockchain.accounts.insert("Alice".to_string(), 100);
     blockchain.accounts.insert("Bob".to_string(), 50);
 
     println!("Account Balances: {:#?}", blockchain.accounts);
 
-    blockchain.mempool.push(Transaction::new(
-        "Alice".to_string(), "Bob".to_string(), 20
-    ));
+    blockchain
+        .mempool
+        .push(Transaction::new("Alice".to_string(), "Bob".to_string(), 20));
+
+    blockchain
+        .mempool
+        .push(Transaction::new("Bob".to_string(), "Alice".to_string(), 10));
 
     blockchain.mempool.push(Transaction::new(
-        "Bob".to_string(), "Alice".to_string(), 10
+        "Bob".to_string(),
+        "Charlie".to_string(),
+        30,
     ));
 
-    blockchain.mempool.push(Transaction::new(
-        "Bob".to_string(), "Charlie".to_string(), 30
-    ));
-
-    blockchain.mempool.push(Transaction::new(
-        "Bob".to_string(), "Bob".to_string(), 30
-    ));
+    blockchain
+        .mempool
+        .push(Transaction::new("Bob".to_string(), "Bob".to_string(), 30));
 
     blockchain.add_block();
 
@@ -60,5 +64,4 @@ fn main() {
 
     let tx_history = blockchain.get_transaction_history(&"Alice".to_string());
     println!("{:#?}", tx_history);
-
 }
